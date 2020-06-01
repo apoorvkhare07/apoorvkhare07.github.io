c808209eb91b870342ab8740298b208a1d0d48fd
apoorv <apoorvkhare007@gmail.com>
2020-06-01 20:11:02 +0530

    Updates

---
+++ precache-manifest.3e28cf3e952b79c6ca88eb4e70116d51.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e201e7b312e8bcf95329c4a2e2968b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "654f5558b5ba189b9a1f",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "66c7e773852e618d3e34",
+    "url": "/static/css/main.f01c63da.chunk.css"
+  },
+  {
+    "revision": "654f5558b5ba189b9a1f",
+    "url": "/static/js/2.af017769.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.af017769.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66c7e773852e618d3e34",
+    "url": "/static/js/main.f2a192b4.chunk.js"
+  },
+  {
+    "revision": "5d5d34e00e1004f1c8d1",
+    "url": "/static/js/runtime-main.7dc3f0cf.js"
+  },
+  {
+    "revision": "285c76a564b76afe9bf88d231ebaeed4",
+    "url": "/static/media/andromeda.285c76a5.png"
+  },
+  {
+    "revision": "e49dfa5c27dd5e2141584b8d3e5fc970",
+    "url": "/static/media/dataphi.e49dfa5c.png"
+  },
+  {
+    "revision": "8285d7efa9c873e457b251b6db985703",
+    "url": "/static/media/dev_cv.8285d7ef.pdf"
+  },
+  {
+    "revision": "6b53d5324242eb54ae04af627258da3a",
+    "url": "/static/media/iitrlogo.6b53d532.png"
+  },
+  {
+    "revision": "377604d2d91a778c561782ae8f4acb88",
+    "url": "/static/media/me.377604d2.jpg"
+  },
+  {
+    "revision": "496a18e0b06f101fb7e688a0eb430d04",
+    "url": "/static/media/theme.496a18e0.png"
+  }
+]);
